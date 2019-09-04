1beb00f84ef1372988951f6c64edc5a149c33323
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 21:46:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  pathway_to_success << "Step 5"
+  pathway_to_success.push("Step 5")
 end
 
 def add_element_to_start_of_array(array, element)
