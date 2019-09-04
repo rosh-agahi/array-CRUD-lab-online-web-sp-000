e7f5dffde4e3df563b559ddbb377a07beb9ac6d2
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 21:44:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  pathway_to_success << "Step 5"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  pathway_to_success.unshift("Begin")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  pathway_to_success.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  pathway_to_success.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
